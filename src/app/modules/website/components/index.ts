10d4fbdecdbb7ad6ed2ddbbaca0b6f95d92c85cb
stephen <sjw948@gmail.com>
2018-08-04 22:28:03 +0100

    bg clipped styles for sectoin intor, section intro component

--- src/app/modules/website/components/index.ts
+++ src/app/modules/website/components/index.ts
@@ -1,3 +1,4 @@
 export { ViewWrapper } from './view-wrapper/view-wrapper.component';
 export { TopNavigation } from './top-navigation/top-navigation.component';
 export { HomePage } from './home-page/home-page.component';
+export { SectionIntro } from './section-intro/section-intro.component';
