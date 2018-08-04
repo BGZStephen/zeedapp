65830a317cd5b1919d3c99105a6c8119bd770df8
stephen <sjw948@gmail.com>
2018-08-04 13:32:36 +0100

    menu toggle handler, window resize event handler

--- src/app/modules/website/components/top-navigation/top-navigation.component.ts
+++ src/app/modules/website/components/top-navigation/top-navigation.component.ts
@@ -5,4 +5,33 @@ import { Component } from "@angular/core";
   templateUrl: './top-navigation.component.html',
   styleUrls: ['./top-navigation.component.scss'],
 })
-export class TopNavigation {}
+export class TopNavigation {
+  mobileMenuVisible: boolean = false;
+
+  toggleMobileMenuVisible():void {
+    this.mobileMenuVisible = !this.mobileMenuVisible;
+    this.setMenuStyle();
+  }
+
+  windowResize():void {
+    if (screen.width > 990 && this.mobileMenuVisible) {
+      this.toggleMobileMenuVisible();
+      this.setMenuStyle();
+    }
+  }
+
+  setMenuStyle():void {
+    const topNav = document.getElementById('top-navigation');
+    const menuUl = topNav.getElementsByTagName('ul')[0];
+    const liHeight = 35;
+    const maxUlHeight = liHeight * 8;
+
+    if (!this.mobileMenuVisible) {
+      menuUl.removeAttribute('style');
+    }
+
+    if (this.mobileMenuVisible && screen.width < 990) {
+      menuUl.style.height = `${maxUlHeight}px`;
+    }
+  }
+}
